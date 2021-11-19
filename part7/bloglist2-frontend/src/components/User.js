1648ec7fbcb1ee1b01277f92f6840b23b9998621
Santeri <sa.virolainen@gmail.com>
2021-11-19 21:52:31 +0200

    finished User list

--- part7/bloglist2-frontend/src/components/User.js
+++ part7/bloglist2-frontend/src/components/User.js
@@ -1,3 +1,4 @@
+import { List } from 'antd'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
@@ -13,11 +14,7 @@ const User = () => {
     <div>
       <h2>User</h2>
       <h3>added blogs</h3>
-      <ul>
-        {user.blogs.map(blog =>
-          <li key={blog.id}>{blog.title}</li>
-        )}
-      </ul>
+      <List dataSource={user.blogs} renderItem={item => <List.Item>{item.title}</List.Item>} />
     </div>
   )
 }
